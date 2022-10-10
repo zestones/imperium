ce6024767c280136fbd522b3e417f0f199c34398
Ghilas Meziane <mezianeghilas3@gmail.com>
2022-10-10 16:19:18 +0200

    Adding Lists and tasks

---
+++ imperium/src/main/java/com/imperium/imperium/model/ProjectList.java
@@ -0,0 +1,32 @@
+package com.imperium.imperium.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "ProjectList")
+@AllArgsConstructor
+public class ProjectList {
+    String title;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "id_project", referencedColumnName = "id")
+    private Project project;
+
+}
