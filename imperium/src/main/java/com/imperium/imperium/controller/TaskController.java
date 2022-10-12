31e2cebd15493f4ae88aea05a124c3dc279e3d88
Iness Bouabid <iness.bouabid2017@gmail.com>
2022-10-12 14:09:59 +0100

    feature/ui-task

--- imperium/src/main/java/com/imperium/imperium/controller/TaskController.java
+++ imperium/src/main/java/com/imperium/imperium/controller/TaskController.java
@@ -1,13 +1,17 @@
 package com.imperium.imperium.controller;
 
+import java.util.ArrayList;
+
 import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.imperium.imperium.model.ProjectList;
 import com.imperium.imperium.model.Task;
 import com.imperium.imperium.service.ProjectList.ProjectListService;
 import com.imperium.imperium.service.task.TaskService;
@@ -34,6 +38,17 @@ public class TaskController {
         t.setList(projectListService.findByTitle(title));
         service.save(t);
         System.out.println("BONJOUR VOICI LA TASK : " + t);
+        model.addAttribute("listTask", t);
         return "redirect:/home/create-task?id="+id;
     }
+
+    @GetMapping(value="/home/create-task/{title}/{id}")
+    public String showTasks(Model model, @PathVariable String title,@PathVariable Long id){
+        ArrayList<Task> list= service.findAll();
+        model.addAttribute("listofTasks", list);
+        return "redirect:/home/create-task?id="+id;
+    }
+
+
+
 }
