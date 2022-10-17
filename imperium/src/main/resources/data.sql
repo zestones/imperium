104c030ccd4448aa58fadbefd14bfd3044ae0666
Idriss <idrissbenguezzou@gmail.com>
2022-10-17 13:13:06 +0200

    feat: populate db with users-generator

---
+++ imperium/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, username, firstname, lastname, password) VALUES
+	(0, 'idriss', 'idriss', 'idriss', '$2b$14$pWzesjrRio2nva0r9770lOscUkqaA14vjskpty5DZkFZ8gomDiYYG'),
+	(1, 'ghilas', 'ghilas', 'ghilas', '$2b$14$QhM9dxgAlwaOQoY/ToaTgu.evNnda1lRLGwpW1sJ.bQZ9XywF/2wa'),
+	(2, 'mohamed', 'mohamed', 'mohamed', '$2b$14$djPE3duQhVLnKbWCyouLqeVi.Vyn7ePuitTD61LYcnNvdwWpjxJJ2');
