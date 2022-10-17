758a34dd770be65ea54cd584360528547aedafc4
Idriss <idrissbenguezzou@gmail.com>
2022-10-17 15:09:46 +0200

    fix(data.sql): id auto generated

--- imperium/src/main/resources/data.sql
+++ imperium/src/main/resources/data.sql
@@ -1,12 +1,16 @@
-INSERT INTO users (id, username, firstname, lastname, password) VALUES
-	(0, 'idriss', 'idriss', 'idriss', '$2b$14$CAmVnSgc7/VVsNu1EWKdHu82Aar79WVsnemti1QCMkCWtwTVcVeHG'),
-	(1, 'mohammed', 'mohammed', 'mohammed', '$2b$14$e60FyOXaPNvH2sARHeVi5.59xEN2gpUltaIACRxB2C1ya4.nOxGBm'),
-	(2, 'ilyes', 'ilyes', 'ilyes', '$2b$14$RfOn2AwBudVZ2YQdPb3Qcur83a5BBaoWG0WH561j4coCMWDd8Cy4y'),
-	(3, 'ghilas', 'ghilas', 'ghilas', '$2b$14$edZfa4HNxpQCDi7aBWeoa.1w.d3jWmNXhSAe2OTrKM/NlnuCDjGv.'),
-	(4, 'iness', 'iness', 'iness', '$2b$14$kGfybl3gKyxG9F0dwA7tHOycc3pNMnjUtWMm/7JXM7BFj03vhNijS'),
-	(5, 'zestones', 'zestones', 'zestones', '$2b$14$.1pOfvWwJzNKPf2o0iboXewu7Fg9uvmO321ODVnYYaY0J3QGcOSGS'),
-	(6, 'mamio', 'mamio', 'mamio', '$2b$14$NTnaewwWelHqyPHS3o5/gO47VqbWgSkTwGtYgRPgsRTDDPnrtmXjy'),
-	(7, 'jaquenet', 'jaquenet', 'jaquenet', '$2b$14$STwvsOQVfC0R46/vw7JrQuObVSgZnqWiR/qkfOvHAErt9yOjtidGO'),
-	(8, 'remie', 'remie', 'remie', '$2b$14$DMeeCG7Zklt6vWRSr3aTBOWOVy.uycHbnNCNhf/0OoUEQYan2mrQm'),
-	(9, 'pierre', 'pierre', 'pierre', '$2b$14$qWoJmVGqzT9k6AvC04V46eE0DAGpomUxLlWRHMacSTj51MkGTOKPW'),
-	(10, 'emonnet', 'emonnet', 'emonnet', '$2b$14$g/tPTvfukou2cWIHFpGglu2f61CMTumnZQMRQ9aQEd.jB.hEuvs5e');
+
+-- populate user table
+INSERT INTO users (username, firstname, lastname, password) VALUES
+	('idriss', 'idriss', 'idriss', '$2b$14$CAmVnSgc7/VVsNu1EWKdHu82Aar79WVsnemti1QCMkCWtwTVcVeHG'),
+	('mohammed', 'mohammed', 'mohammed', '$2b$14$e60FyOXaPNvH2sARHeVi5.59xEN2gpUltaIACRxB2C1ya4.nOxGBm'),
+	('ilyes', 'ilyes', 'ilyes', '$2b$14$RfOn2AwBudVZ2YQdPb3Qcur83a5BBaoWG0WH561j4coCMWDd8Cy4y'),
+	('ghilas', 'ghilas', 'ghilas', '$2b$14$edZfa4HNxpQCDi7aBWeoa.1w.d3jWmNXhSAe2OTrKM/NlnuCDjGv.'),
+	('iness', 'iness', 'iness', '$2b$14$kGfybl3gKyxG9F0dwA7tHOycc3pNMnjUtWMm/7JXM7BFj03vhNijS'),
+	('zestones', 'zestones', 'zestones', '$2b$14$.1pOfvWwJzNKPf2o0iboXewu7Fg9uvmO321ODVnYYaY0J3QGcOSGS'),
+	('mamio', 'mamio', 'mamio', '$2b$14$NTnaewwWelHqyPHS3o5/gO47VqbWgSkTwGtYgRPgsRTDDPnrtmXjy'),
+	('jaquenet', 'jaquenet', 'jaquenet', '$2b$14$STwvsOQVfC0R46/vw7JrQuObVSgZnqWiR/qkfOvHAErt9yOjtidGO'),
+	('remie', 'remie', 'remie', '$2b$14$DMeeCG7Zklt6vWRSr3aTBOWOVy.uycHbnNCNhf/0OoUEQYan2mrQm'),
+	('pierre', 'pierre', 'pierre', '$2b$14$qWoJmVGqzT9k6AvC04V46eE0DAGpomUxLlWRHMacSTj51MkGTOKPW'),
+	('emonnet', 'emonnet', 'emonnet', '$2b$14$g/tPTvfukou2cWIHFpGglu2f61CMTumnZQMRQ9aQEd.jB.hEuvs5e');
+
+-- populate project table
