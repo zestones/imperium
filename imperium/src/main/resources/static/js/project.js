17405376da07702e8a7305deac20cf047fa805f8
Idriss <idrissbenguezzou@gmail.com>
2022-12-15 06:28:18 +0100

    feat: edit task from side modal + fix header project & home

--- imperium/src/main/resources/static/js/project.js
+++ imperium/src/main/resources/static/js/project.js
@@ -1,3 +1,4 @@
+// DRAG AND DROP TASK
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
@@ -12,10 +13,21 @@ function drop(ev) {
   ev.currentTarget.appendChild(document.getElementById(data));
 }
 
+// OPEN / CLOSE TASK CREATION
 function openCreateTask(val) {
-   document.getElementById('create-task-container' + val).style.display = "block";
+  document.getElementById('create-task-container' + val).style.display = "block";
 }
 
 function closeCreateTask(val) {
-   document.getElementById('create-task-container' + val).style.display = "none";
+  document.getElementById('create-task-container' + val).style.display = "none";
+}
+
+// SIDE MODAL TO EDIT TASK
+function openNav(val) {
+  console.log('edit-task-modal' + val)
+  document.getElementById('edit-task-modal' + val).style.width = "25%";
+}
+
+function closeNav(val) {
+  document.getElementById('edit-task-modal' + val).style.width = "0";
 }
